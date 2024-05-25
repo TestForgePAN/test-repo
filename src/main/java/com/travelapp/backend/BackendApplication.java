79940b799c9fd638621a0b99b99d8002cbe4f728
DeepeshDragoneel <deepeshash444@gmail.com>
2024-05-25 19:14:59 +0530

    commit5

--- src/main/java/com/travelapp/backend/BackendApplication.java
+++ src/main/java/com/travelapp/backend/BackendApplication.java
@@ -10,7 +10,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @EnableSwagger2
 @SpringBootApplication
 public class BackendApplication {
-	// dummy commit - 2
+	// dummy commit - 3
 	public static void main(String[] args) {
 		SpringApplication.run(BackendApplication.class, args);
 	}
